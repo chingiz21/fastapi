60dc6e5a56b650b651c49908ed7a501b9f2eecdb
Chingiz Zaripov <chingiz.zaripov2000@gmail.com>
2023-03-06 17:58:42 +0200

    content type;

--- src/api.js
+++ src/api.js
@@ -7,6 +7,7 @@ const app = express();
 app.all('/*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Content-Type", "application/json")
     next();
 });
 
